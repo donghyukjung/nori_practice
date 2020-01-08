5a8960023648468360fafb54e5a6acef159f1f47
DonghyukJung <dhjung081121@gmail.com>
2020-01-08 16:43:30 +0900

    200108 PA3-2.1 Complete

--- src/simple.cpp
+++ src/simple.cpp
@@ -6,25 +6,29 @@ NORI_NAMESPACE_BEGIN
 class SimpleIntegrator : public Integrator {
 public:
 	SimpleIntegrator(const PropertyList &props) {
-		/* No parameters this time */
+		p = props.getPoint("position");
+		psi = props.getColor("energy");
 	}
-
 
 	Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
 		/* Find the surface that is visible in the requested direction */
 		Intersection its;
-		if (!scene->rayIntersect(ray, its))
+		Point3f x = its.p;
+		const Ray3f shadow = Ray3f(x, -(x - p));
+		if (!scene->rayIntersect(ray, its) || scene->rayIntersect(shadow))
 			return Color3f(0.0f);
-
-		/* Return the component-wise absolute
-		   value of the shading normal as a color */
-		Normal3f n = its.shFrame.n.cwiseAbs();
-		return Color3f(n.x(), n.y(), n.z());
+		
+		return psi / (4 * pow(M_PI, 2))
+			*(std::max(0.0f, its.shFrame.n.dot(p - x) / (its.shFrame.n.norm() * (p - x).norm()))
+			/ (x - p).dot(x - p));
 	}
-
+	
+	
 	std::string toString() const {
 		return "SimpleIntegrator[]";
 	}
+	Point3f p;
+	Color3f psi;
 };
 
 NORI_REGISTER_CLASS(SimpleIntegrator, "simple");
